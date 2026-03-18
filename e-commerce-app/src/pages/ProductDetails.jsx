74e1de45042930c0e6dca90939d9b6229714495d
KaegenGov1998 <152480939+KaegenGov1998@users.noreply.github.com>
2026-03-18 15:03:17 +0200

    adjusted productDetails.jsx cart to fix image issue

--- e-commerce-app/src/pages/ProductDetails.jsx
+++ e-commerce-app/src/pages/ProductDetails.jsx
@@ -41,7 +41,7 @@ export default function ProductPage() {
         <div className="flex flex-col pl-10">
           <Link to="/">
             <div className="h-22 mt-12 flex text-xl items-center gap-4 font-400">
-              <img src={backIcon} alt="back button" className="h-4"/>
+              <img src={backIcon} alt="back button" className="h-4" />
               Back
             </div>
           </Link>
@@ -99,20 +99,24 @@ export default function ProductPage() {
         </div>
         {/* Bag Section */}
 
-        <div className="w-64 border-l-2 border-black pl-6 ml-15 mt-12">
+        <div className="w-64 border-l-2 border-black pl-6">
           <h2 className="text-xl font-bold mb-6 text-center">Bag</h2>
 
           <div className="grid grid-cols-3 gap-3 mb-6">
-            {cart.map((item) => (
-              <img
-                key={item.id}
-                src={item.image}
-                className="w-14 h-14 object-contain bg-gray-100 rounded-lg"
-              />
-            ))}
+            {cart.map((item) => {
+              const product = products.find((p) => p.id === item.id);
+              return (
+                <img
+                  key={item.id}
+                  src={product?.image}
+                  alt={item.name}
+                  className="w-14 h-14 object-contain bg-gray-100 rounded-lg"
+                />
+              );
+            })}
           </div>
 
-          <Link to="/checkout">
+          <Link to="/bag">
             <button className="w-full bg-black text-white p-3 rounded-xl flex items-center justify-center gap-2">
               <img src={icon} className="w-6 h-6" />
               View Bag
