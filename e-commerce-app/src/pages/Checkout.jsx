f7824efd579e0c65d06f23bb8d8891b7a36c69b7
Lehlohonolo Xaba <lxaba1305@gmail.con>
2026-03-18 15:06:16 +0200

    Completed Checkout Section

--- e-commerce-app/src/pages/Checkout.jsx
+++ e-commerce-app/src/pages/Checkout.jsx
@@ -1,6 +1,4 @@
-import Sidebar from "../components/Sidebar";
 import products from "../data/products";
-import DellXPS13 from "../assets/Dell XPS 13.png"
 import card from "../assets/card.png"
 import gift from "../assets/gift.png"
 
@@ -8,14 +6,22 @@ import gift from "../assets/gift.png"
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { increaseQty, decreaseQty } from "../redux/cartSlice";
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom"
+
+
 const Checkout = () => {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.cart.items);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [billingChecked, setBillingChecked] = useState(true);
 
-  const subtotal = items.reduce(
+  const enrichedItems = items.map((item) => {
+    const product = products.find((p) => p.id === item.id);
+    return { ...item, image: product?.image };
+  });
+
+  const subtotal = enrichedItems.reduce(
     (sum, item) => sum + item.price * item.quantity,
     0,
   );
@@ -24,13 +30,26 @@ const Checkout = () => {
   const gst = subtotal * 0.13;
   const total = subtotal + shipping + gst;
 
+  const renderStars = (rating) => {
+    const full = Math.floor(rating);
+    const half = rating % 1 >= 0.5;
+    return (
+        <div className="flex items-center gap-1">
+            {[...Array(full)].map((_, i) => (
+                <span key={i} className="text-green-700 text-xl">★</span>
+            ))}
+            {half && <span className="text-green-700 text-xl">✩</span>}
+            <span className="text-green-700 ml-2 text-sm"> {rating} / 5 </span>
+            </div>
+            )
+  }
+
 
   return (
-    <div className="bg-gray-100 min-h-screen flex p-6 gap-6">
+    <div className="bg-gray-100 min-h-screen p-6 gap-6">
 
-<Sidebar />
 
-    <div className="grid grid-cols-3 flex-1 gap-10">
+    <div className="grid grid-cols-3 gap-10">
       <div className="col-span-2 space-y-8">
         <div className="bg-white rounded-2xl p-8 flex justify-between items-start">
           <div>
@@ -46,60 +65,78 @@ const Checkout = () => {
 
           </div>
 
-          <button className="border px-6 py-2 rounded-lg">
+
+          <Link to="/add-address">
+          <button className="border px-6 py-2 rounded-full">
             Change
           </button>
-
+         </Link>
         </div>
 
-        <div className="bg-white rounded-2xl p-8 flex justify-between">
+        <div className="bg-white rounded-2xl p-8 flex justify-between items-start">
 
           <div>
           <h2 className="text-2xl tracking-widest font-semibold mb-6">
             PAYMENT METHOD
           </h2>
 
-          <p className="mb-2">
-            💳 Mastercard ending in 1252
-          </p>
+         <div className="flex items-center gap-3 mb-3">
+          <img src={card} className="w-6 h-6" alt="card" />
+          <span>
+            <span className="font-semibold">Mastercard</span>{" "}
+            <span className="text-gray-400">ending in 1252</span>
+          </span>
+         </div>
 
-          <p className="mb-2">
-            🎁 $53.21 gift card balance
-          </p>
+         <div className="flex items-center gap-3 mb-3">
+          <img src={gift} className="w-6 h-6" alt="gift" />
+          <span>
+            <span className="font-semibold">$ 53.21</span>{" "}
+            <span className="text-gray-400">gift card balance</span>
+          </span>
+         </div>
 
-          <p>
-            ☑ Billing address same as Shipping Address
-          </p>
+          <div className="flex items-center gap-3">
+            <div 
+            onClick={() => setBillingChecked(!billingChecked)}
+            className={`transition-all duration-200 rounded cursor-pointer border-2 ${
+              billingChecked
+              ? "w-5 h-6 bg-green-700 border-green-700"
+              : "w-6 h-6 bg-white border-gray-400"
+            }`} />
+            <span>Billing address same as Shipping Address</span>
+          </div>
           </div>
 
+        <Link to="/add-payment">
           <button className="border px-6 py-2 rounded-lg">
             Change
           </button>
-
+</Link>
         </div>
-
+        
         <div className="bg-white rounded-2xl p-8">
 
           <h2 className="text-2xl tracking-widest font-semibold mb-8">
             REVIEW YOUR BAG
           </h2>
 
-       {items.map((item) => (
+       {enrichedItems.map((item) => (
   <div key={item.id} className="mb-10">
 
     <div className="flex justify-between items-center">
 
-      {/* LEFT SIDE */}
+      {/* Left side */}
       <div className="flex gap-8">
 
-        {/* IMAGE */}
+        {/* Image */}
         <img
           src={item.image}
           className="w-32 h-32 object-contain"
           alt={item.name}
         />
 
-        {/* TEXT CONTENT */}
+        {/* Text */}
         <div className="flex flex-col justify-center">
 
           <h2 className="text-2xl font-semibold">
@@ -115,11 +152,8 @@ const Checkout = () => {
           </p>
 
           {/* RATING */}
-          <div className="flex items-center gap-3 mt-3 text-green-700 text-lg">
-            <span>★ ★ ★ ★ ☆</span>
-            <span className="text-gray-600 text-base">
-              {item.rating} / 5
-            </span>
+          <div className=" mt-3">
+           {renderStars(item.rating)}
           </div>
 
           {/* PRICE */}
@@ -160,7 +194,9 @@ const Checkout = () => {
 ))}
         </div>
       </div>
-      <div className="bg-white p-8 rounded-2xl shadow h-fit">
+
+      <div>
+      <div className="bg-white p-8 rounded-2xl shadow mb-4">
         <h2 className="text-xl font-bold mb-6">Order Summary</h2>
 
         <div className="flex justify-between mb-3">
@@ -186,18 +222,21 @@ const Checkout = () => {
         <hr className="my-4" />
 
          <div className="flex justify-between mb-3">
-          <span>Order Total:</span>
-          <span>${total.toFixed(2)}</span>
+          <span className="font-semibold text-red-500">Order Total:</span>
+          <span className="font-semibold text-red-500">${total.toFixed(2)}</span>
         </div>
 
        <button
          onClick={() => navigate("/add-payment")}
-         className="w-full bg-green-900 text-white py-3 rounded-lg mb-4">
+         className="w-full bg-black text-white py-3 rounded-lg mb-4">
             Place your Order
        </button>
+       </div>
         
-        <Link to="/bag" className="block text-center border border-gray-400 py-2 rounded-lg">
+        <Link to="/bag" className=" flex justify-end mt-4">
+        <button className="border border-gray-400 py-2 px-8 rounded-full">
           ← Back
+          </button>
         </Link>
       </div>
     </div>
